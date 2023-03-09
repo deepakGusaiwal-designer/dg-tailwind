b28d13b9318a4ef788a822a55577f51fa2e5a7ea
Deepak G <deepakgusaiwal07@gmail.com>
2023-03-09 18:47:56 +0530

    gtag added

--- components/SEO.js
+++ components/SEO.js
@@ -1,11 +1,26 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function SEO({ title, description }) {
   return (
-    <Head>
-      <title>Deepak Gusaiwal</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-    </Head>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-4Z4ZKL03MX`}
+      />
+      <Script id="gt-tag" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4Z4ZKL03MX');
+        `}
+      </Script>
+      <Head>
+        <title>Deepak Gusaiwal</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+      </Head>
+    </>
   );
 }
